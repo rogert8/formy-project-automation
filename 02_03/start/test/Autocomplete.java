7bd4acb0e24a4902c31201595290b961ef3c0a3d
rogert8 <68079928+rogert8@users.noreply.github.com>
2021-10-28 00:52:39 -0700

    Initial commit

---
+++ 02_03/start/test/Autocomplete.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) throws InterruptedException {
+
+        // System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        // autocomplete.click();
+        autocomplete.sendKeys("4911 Kona Drive, Huntington Beach, CA");
+        Thread.sleep(1000);
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
+        driver.quit();
+    }
+}
---
+++ 06_04/start/test/Form.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.junit.Assert.assertEquals;
+
+public class Form {
+    public static void main(String[] args) {
+
+        // System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/form");
+
+        FormPage formPage = new FormPage();
+        formPage.submitForm(driver);
+
+        ConfirmationPage confirmationPage = new ConfirmationPage();
+        confirmationPage.waitForAlertBanner(driver);
+
+        assertEquals("The form was successfully submitted!", confirmationPage.getAlertBannerText(driver));
+
+        driver.quit();
+    }
+}
